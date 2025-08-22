09f4f2a718f72335e88200a5e0a4939fed38d441
PENG Bo <pbdm@foxmail.com>
2025-08-22 21:17:51 +0800

    fix

--- src/endpoints/custom.ts
+++ src/endpoints/custom.ts
@@ -43,49 +43,28 @@ export class Custom extends OpenAPIRoute {
 		},
 	};
 
-	async handle(c: AppContext) {
+		async handle(c: AppContext) {
 
-		const data = await this.getValidatedData<typeof this.schema>();
-		const { url } = data.params;
+			// Get validated data
+			const data = await this.getValidatedData<typeof this.schema>();
+			const { url } = data.params;
+			// 示例 URL
+			// const url = `https://api.live.bilibili.com/xlive/web-ucenter/v1/user_title/GetTitles`;
+			
+			const resp = await fetch(decodeURIComponent(url));
 
-		const originalRequest = c.req.raw
+			if (!resp.ok) {
+				return Response.json({
+					success: false,
+					error: `Failed to fetch: ${resp.status}`,
+				}, { status: resp.status });
+			}
+			const ret = await resp.json();
 
-		// 拷贝一份可变的 headers
-		const headers = new Headers(originalRequest.headers)
-
-		// 删除条件请求头
-		headers.delete('If-None-Match')
-		headers.delete('If-Modified-Since')
-		headers.delete('If-Match')
-
-		// 构造一个新的 Request（Hono 的 c.req.raw 是 immutable 的，所以必须新建）
-		const newRequest = new Request(decodeURIComponent(url), {
-			method: originalRequest.method,
-			headers,
-			body: originalRequest.body,  
-		})
-
-		// 发送请求
-		const resp = await fetch(newRequest)
-		
-
-		// const resp = await fetch(decodeURIComponent(url));
-
-		if (!resp.ok) {
-			return Response.json({
-				success: false,
-				error: `Failed to fetch: ${resp.status}`,
-			}, { status: resp.status });
+			// 直接返回外部接口内容
+			return {
+				success: true,
+				RETURN: ret
+			};
 		}
-		const ret = await resp.json();
-
-		const responseHeaders = Object.fromEntries(resp.headers)
-		// console.log('源站响应头:', responseHeaders)
-
-		// 直接返回外部接口内容
-		return {
-			success: true,
-			RETURN: ret
-		};
-	}
 }
