da8f8c0f1d71400848aa395b26548a79c1ff9fd2
PENG Bo <pbdm@foxmail.com>
2025-08-22 20:28:00 +0800

    add custom url proxy

---
+++ src/endpoints/custom.ts
@@ -0,0 +1,69 @@
+import { Bool, OpenAPIRoute, Str } from "chanfana";
+import { z } from "zod";
+import { type AppContext, Task } from "../types";
+
+export class Custom extends OpenAPIRoute {
+	schema = {
+		tags: ["custom"],
+		summary: "custom",
+		request: {
+			params: z.object({
+				url: Str({ description: "parse url" }),
+			}),
+		},
+		responses: {
+			"200": {
+				description: "Returns room info if found",
+				content: {
+					"application/json": {
+						schema: z.object({
+							series: z.object({
+								success: Bool(),
+								result: z.object({
+									task: Task,
+								}),
+							}),
+						}),
+					},
+				},
+			},
+			"404": {
+				description: "url found",
+				content: {
+					"application/json": {
+						schema: z.object({
+							series: z.object({
+								success: Bool(),
+								error: Str(),
+							}),
+						}),
+					},
+				},
+			},
+		},
+	};
+
+		async handle(c: AppContext) {
+
+			// Get validated data
+			const data = await this.getValidatedData<typeof this.schema>();
+			const { url } = data.params;
+			// 示例 URL
+			// const url = `https://api.live.bilibili.com/xlive/web-ucenter/v1/user_title/GetTitles`;
+			const resp = await fetch(decodeURIComponent(url));
+
+			if (!resp.ok) {
+				return Response.json({
+					success: false,
+					error: `Failed to fetch: ${resp.status}`,
+				}, { status: resp.status });
+			}
+			const ret = await resp.json();
+
+			// 直接返回外部接口内容
+			return {
+				success: true,
+				RETURN: ret
+			};
+		}
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { TaskCreate } from "./endpoints/taskCreate";
 import { TaskDelete } from "./endpoints/taskDelete";
 import { TaskFetch } from "./endpoints/taskFetch";
 import { TaskList } from "./endpoints/taskList";
+import { Custom } from "./endpoints/custom";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
@@ -18,6 +19,7 @@ openapi.get("/api/tasks", TaskList);
 openapi.post("/api/tasks", TaskCreate);
 openapi.get("/api/tasks/:taskSlug", TaskFetch);
 openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+openapi.get("/api/custom/:url", Custom);
 
 // You may also register routes for non OpenAPI directly on Hono
 // app.get('/test', (c) => c.text('Hono!'))
